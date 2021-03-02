bfe3bd66bb809c38ed2c09f4a8a1aeacb92ec965
ElektroTekniska Föreningen Administratör <info@etf.nu>
2021-03-02 16:35:56 +0100

    Upgraded from Bootstrap 2 to Bootstrap 5

--- ping.php
+++ ping.php
@@ -6,11 +6,11 @@ $output = shell_exec('ping -c1 -W1 192.168.10.'.$ip);
 preg_match('/time=([\.0-9]+)/', $output, $m);
 
 if(empty($m)) {
-	echo "<span class=\"badge badge-important\">down</span>";
+	echo "<span class=\"badge bg-danger\">down</span>";
 } else {
 	if ((float)$m[1] > 10) {
-		echo "<span class=\"badge badge-warning\">".$m[1]."</span>";
+		echo "<span class=\"badge bg-warning\">".$m[1]."</span>";
 	} else {
-		echo "<span class=\"badge badge-success\">".$m[1]."</span>";
+		echo "<span class=\"badge bg-success\">".$m[1]."</span>";
 	}
 }
